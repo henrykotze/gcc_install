950e24f9f7898f5bb1c4178910a144ae65aa7895
Henry Kotze <henry@localhost.localdomain>
2017-12-22 17:47:29 +0200

    move install direcory to $USER/

--- install_gcc.sh
+++ install_gcc.sh
@@ -4,7 +4,7 @@
 echo "Downloading and Installing gcc dependencies"
 sudo yum install gcc gcc-c++ gcc-gfortran gcc-go gcc-objc gcc-objc++ tar wget gcc-gnat gzip bzip2 -y
 GCC_VERSION="gcc-7.2.0"
-PREFIX=/home/henry/$GCC_VERSION
+PREFIX=/home/$USER/$GCC_VERSION
 
 echo "Downloading ${GCC_VERSION} from FTP server"
 #Downloading gcc from germany ftp server
